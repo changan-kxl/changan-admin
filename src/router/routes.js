5114b13abd3a9d7bb5e5dd7ef77306914f969007
changan <740394554@qq.com>
2021-07-28 14:08:25 +0800

    Fix Bug

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,4 @@
-import Main from '@/components/layout/Main.vue'
+import Main from '../components/layout/Main.vue'
 import CommerViews from '@/components/commerViews.vue'
 import { routes as tablesRoutes } from './tables'
 /**
